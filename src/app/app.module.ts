0238cff2c4bdccd0f8d4d9e64bc5cbfe3442ea2d
shagat <shagat.bing@gmail.com>
2022-06-11 22:40:09 +0530

    add http client into the project and created new data.sevice that will handle all data related services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -38,6 +39,7 @@ import { RecipeService } from './recipes/recipe.service';
     FormsModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
