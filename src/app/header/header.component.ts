893c0bdba2b26df4fa8ba01af32393289aa20326
shagat <shagat.bing@gmail.com>
2022-06-11 22:51:48 +0530

    add PUT data method in the data.storage.services that gets triggered when click on 'save data' button of the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
-    selector:'app-header',
-    templateUrl:'header.component.html'
+    selector: 'app-header',
+    templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
     collapsed = true;
+
+    constructor(private dataStorageService: DataStorageService) { }
+    
+    onSaveData() {
+        this.dataStorageService.storeRecipes();
+    }
 }
