6c1d105c1b5fe1bcc14d6b4590c00389758c3f3b
shagat <shagat.bing@gmail.com>
2022-06-12 14:00:47 +0530

    added headers manage dropdown functions-to save data to server and fetch data from server

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,24 +9,31 @@ export class RecipeService {
     recipeChanged = new Subject<Recipe[]>();
     constructor(private slService: ShoppingListService) { }
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A very tasty meat with onion',
-            'This is a very delightful tropical dinner',
-            'https://images.unsplash.com/photo-1504674900247-0877df9cc836?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe(
-            'A very tasty meat',
-            'This is a very delightful dinner',
-            'https://images.unsplash.com/photo-1504674900247-0877df9cc836?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
-            [
-                new Ingredient('Meat', 3),
-                new Ingredient('French Fries', 30)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A very tasty meat with onion',
+    //         'This is a very delightful tropical dinner',
+    //         'https://images.unsplash.com/photo-1504674900247-0877df9cc836?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe(
+    //         'A very tasty meat',
+    //         'This is a very delightful dinner',
+    //         'https://images.unsplash.com/photo-1504674900247-0877df9cc836?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
+    //         [
+    //             new Ingredient('Meat', 3),
+    //             new Ingredient('French Fries', 30)
+    //         ])
+    // ];
+    private recipes: Recipe[] = [];
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipeChanged.next(this.recipes.slice());
+    }
+
     getRecipes() {
         return this.recipes.slice();
     }
