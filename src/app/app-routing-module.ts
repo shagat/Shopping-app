38b3084b0a62751b578dea7de83bd6db9fe78add
shagat <shagat.bing@gmail.com>
2022-06-12 22:14:01 +0530

    add authentication form template and its functionality

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -6,6 +6,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -16,6 +17,7 @@ const appRoutes: Routes = [
         {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] },
     ]},
     { path: 'shopping-list', component: ShoppingListComponent },   
+    { path: 'login', component: AuthComponent},   
 ]
 
 @NgModule({
