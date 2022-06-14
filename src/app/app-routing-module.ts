b975d4189b20a76e8749e43a13f740b6a91dd043
shagat <shagat.bing@gmail.com>
2022-06-14 22:06:56 +0530

    using routerGuard to protect routes that needs login

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -7,10 +7,13 @@ import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, children: [
+    { path: 'recipes', component: RecipesComponent,
+    canActivate:[AuthGuard],
+    children: [
         {path: '', component: RecipeStartComponent },
         {path: 'new', component: RecipeEditComponent },
         {path: ':id', component: RecipeDetailsComponent, resolve: [RecipesResolverService] },
