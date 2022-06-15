d8427a70ac7fad94027daa1474e6302252c96beb
shagat <shagat.bing@gmail.com>
2022-06-15 23:30:21 +0530

    removed the redundant imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { RecipesModule } from './recipes/recipes.module';
 import { AppRoutingModule } from './app-routing-module';
-import { ShoppingModule } from './shopping-list/shopping.module';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
+
 
 
 
