b5bbf07507e4c0e0b975c3ad22fb08a498db3678
shagat <shagat.bing@gmail.com>
2022-06-30 11:11:25 +0530

    took out shopping list service and added store

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
 import { Recipe } from "./recipe.model";
 import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions'
 import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
@@ -10,26 +10,8 @@ import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer'
 @Injectable()
 export class RecipeService {
     recipeChanged = new Subject<Recipe[]>();
-    constructor(private slService: ShoppingListService, private store: Store<fromShoppingList.AppState>) { }
+    constructor(private store: Store<fromShoppingList.AppState>) { }
 
-    // private recipes: Recipe[] = [
-    //     new Recipe(
-    //         'A very tasty meat with onion',
-    //         'This is a very delightful tropical dinner',
-    //         'https://images.unsplash.com/photo-1504674900247-0877df9cc836?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
-    //         [
-    //             new Ingredient('Meat', 1),
-    //             new Ingredient('French Fries', 20)
-    //         ]),
-    //     new Recipe(
-    //         'A very tasty meat',
-    //         'This is a very delightful dinner',
-    //         'https://images.unsplash.com/photo-1504674900247-0877df9cc836?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
-    //         [
-    //             new Ingredient('Meat', 3),
-    //             new Ingredient('French Fries', 30)
-    //         ])
-    // ];
     private recipes: Recipe[] = [];
 
     setRecipes(recipes: Recipe[]) {
