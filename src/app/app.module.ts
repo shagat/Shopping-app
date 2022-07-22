38931b6f7fd2d77bb233ae26962a4de4828b043b
shagat <shagat.bing@gmail.com>
2022-07-22 20:08:59 +0530

    added auth effect logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing-module';
@@ -12,6 +13,7 @@ import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import * as fromApp from './store/app.reducer'
+import { AuthEffects } from './auth/store/auth.effects';
 
 
 
@@ -26,6 +28,7 @@ import * as fromApp from './store/app.reducer'
     AppRoutingModule,
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects]),
     SharedModule,
     CoreModule,
     StoreModule.forRoot({}, {}),
--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,8 +1,9 @@
 import { Action } from "@ngrx/store"
 
-export const LOGIN_START = '[Auth] Login Start'
-export const LOGIN = '[Auth] Login'
-export const LOGOUT = '[Auth] Logout'
+export const LOGIN_START = '[Auth] Login Start';
+export const LOGIN = '[Auth] Login';
+export const LOGIN_FAIL = '[Auth] Login Fail';
+export const LOGOUT = '[Auth] Logout';
 
 export class Login implements Action {
     readonly type = LOGIN;
@@ -18,6 +19,12 @@ export class Login implements Action {
 
 export class Logout implements Action {
     readonly type = LOGOUT;
+}
+
+export class LoginStart implements Action {
+    readonly type = LOGIN_START;
+
+    constructor(public payload: {email:string, password: string}){}
 }
 
 export type AuthActions =
