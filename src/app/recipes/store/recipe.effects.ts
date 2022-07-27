11464c1d4b802da23b107b06044a48fa5b7c234c
shagat <shagat.bing@gmail.com>
2022-07-27 20:18:27 +0530

    added recipe store migration codes

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,42 +1,53 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Actions, Effect, ofType } from "@ngrx/effects";
-import { Store } from "@ngrx/store";
-import { switchMap, map, withLatestFrom } from "rxjs/operators";
-import { Recipe } from "../recipe.model";
-import * as RecipesActions from './recipe.actions'
-import * as fromApp from '../../store/app.reducer'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { switchMap, map, withLatestFrom } from 'rxjs/operators';
+import { Recipe } from '../recipe.model';
+import * as RecipesActions from './recipe.actions';
+import * as fromApp from '../../store/app.reducer';
 
 @Injectable()
 export class RecipeEffects {
+  fetchRecipes$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(RecipesActions.FetchRecipes),
+      switchMap(() => {
+        return this.http.get<Recipe[]>(
+          'https://ng-recipe-book-a38be-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
+        );
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      map((recipes) => {
+        return RecipesActions.SetRecipes({ recipes });
+      })
+    )
+  );
 
-    @Effect()
-    fetchRecipes = this.action$.pipe(
-        ofType(RecipesActions.FETCH_RECIPES),
-        switchMap(() => {
-            return this.http
-                .get<Recipe[]>(
-                    'https://ng-recipe-book-a38be-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
-                )
-        }), map(recipes => {
-            return recipes.map(recipe => {
-                return {
-                    ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
-                };
-            })
-        }), map(recipes => {
-            return new RecipesActions.SetRecipes(recipes);
-        })
-    );
-
-    @Effect({ dispatch: false })
-    storeRecipes = this.action$.pipe(ofType(RecipesActions.STORE_RECIPES),
+  storeRecipes$ = createEffect(
+    () =>
+      this.action$.pipe(
+        ofType(RecipesActions.StoreRecipes),
         withLatestFrom(this.store.select('recipes')),
         switchMap(([actionData, recipesState]) => {
-            return this.http.put(
-                'https://ng-recipe-book-a38be-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json',
-                recipesState.recipes)
+          return this.http.put(
+            'https://ng-recipe-book-a38be-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json',
+            recipesState.recipes
+          );
         })
-    )
-    constructor(private action$: Actions, private http: HttpClient, private store: Store<fromApp.AppState>) { }
+      ),
+    { dispatch: false }
+  );
+  constructor(
+    private action$: Actions,
+    private http: HttpClient,
+    private store: Store<fromApp.AppState>
+  ) {}
 }
--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,50 +1,36 @@
-import { Recipe } from "../recipe.model";
-import * as RecipesActions from './recipe.actions'
+import { Action, createReducer, on } from '@ngrx/store';
+import { Recipe } from '../recipe.model';
+import * as RecipesActions from './recipe.actions';
 
 export interface State {
-    recipes: Recipe[];
+  recipes: Recipe[];
 }
 
 const initialState: State = {
-    recipes: []
-}
+  recipes: [],
+};
+const _recipeReducer = createReducer(
+  initialState,
+  on(RecipesActions.SetRecipes, (state, action) => ({
+    ...state,
+    recipes: [...action.recipes],
+  })),
+  on(RecipesActions.AddRecipe, (state, action) => ({
+    ...state,
+    recipes: [...state.recipes, action.recipe],
+  })),
+  on(RecipesActions.UpdateRecipe, (state, action) => ({
+    ...state,
+    recipes: state.recipes.map((recipe, index) =>
+      index === action.index ? { ...action.recipe } : recipe
+    ),
+  })),
+  on(RecipesActions.DeleteRecipe, (state, action) => ({
+    ...state,
+    recipes: state.recipes.filter((_, index) => index !== action.index),
+  }))
+);
 
-export function recipeReducer(state = initialState, action: RecipesActions.RecipesActions) {
-    switch (action.type) {
-        case RecipesActions.SET_RECIPES:
-            return {
-                ...state,
-                recipes: [...action.payload]
-            };
-
-        case RecipesActions.ADD_RECIPE:
-            return {
-                ...state,
-                recipes: [...state.recipes, action.payload]
-            };
-
-        case RecipesActions.UPDATE_RECIPE:
-            const updatedRecipe = {
-                ...state.recipes[action.payload.index],
-                ...action.payload.newRecipe
-            };
-
-            const updatedRecipes = [...state.recipes];
-            updatedRecipes[action.payload.index] = updatedRecipe;
-
-            return {
-                ...state,
-                recipes: [updatedRecipes]
-            };
-
-        case RecipesActions.DELETE_RECIPE:
-            return {
-                ...state,
-                recipes: state.recipes.filter((recipe, index) => {
-                    return index !== action.payload;
-                })
-            };
-        default:
-            return state;
-    }
+export function recipeReducer(state: State, action: Action){
+  return _recipeReducer(state,action);
 }
